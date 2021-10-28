9c69282cf6e9f731e6e999ca28a1646677c82700
Hyeon Yu <hyeon@ualberta.ca>
2021-10-28 15:00:13 +0900

    mode for seg, det, and only nifti

--- to_coco.py
+++ to_coco.py
@@ -9,6 +9,9 @@ from torchvision.transforms import Compose
 import torch
 import json
 from datetime import date
+import random
+import shutil
+import argparse
 
 '''
 For segmentation.
@@ -32,8 +35,10 @@ root
 '''
 
 def create_dir(path):
-    if not os.path.exists(path):
-        os.makedirs(path)
+    if os.path.exists(path):
+        shutil.rmtree(path)
+
+    os.makedirs(path)
 
 
 class CLAHE():
@@ -76,22 +81,15 @@ class Rescale():
         return img
 
 
-class ToTensor():
-
-    def __call__(self, img):
-        return torch.from_numpy(img)
-
-
 def preprocess_image(img):
     preprocessing = Compose([
         CLAHE(),
         Rescale((512, 512)),
-        ToTensor()
     ])
 
-    return preprocessing(img.copy()).unsqueeze(0)
+    return preprocessing(img.copy())
 
-#min max noemalization
+
 def dcm_to_png(dcom):
     dcm_data = dcm.read_file(dcom, force=True)
 
@@ -99,40 +97,42 @@ def dcm_to_png(dcom):
         if dcm_data.PhotometricInterpretation != "MONOCHROME2":
             image = np.invert(dcm_data.pixel_array.squeeze())
         else:
-            image = dcm_data.pxel_array.squeeze()
+            image = dcm_data.pixel_array.squeeze()
     except:
         dcm_data.file_meta.TransferSyntaxUID = dcm.uid.ImplicitVRLittleEndian
         if dcm_data.PhotometricInterpretation != "MONOCHROME2":
             image = np.invert(dcm_data.pixel_array.squeeze())
         else:
             image = dcm_data.pixel_array.squeeze()
-    image = (image - image.min()) / (image.max() - image.min())
 
+    image = (image - image.min()) / (image.max() - image.min())
     input_tensor = preprocess_image(image)
 
-    return np.array(input_tensor).squeeze().transpose(1,2,0)
+    return input_tensor.transpose(1,2,0)
 
 
 def make_img(dcm_file, coco_dir, used_for):
     img = np.uint8(dcm_to_png(dcm_file)*255)
     cv2.imwrite(f"{coco_dir}/{used_for}/{dcm_file.split('/')[-1].split('.')[0]}.png",img)
+
     return img.shape[0], img.shape[1], img
 
 
 def nii_to_seg(mask, w, h):
+    seg = []
+
     gt = nib.load(mask)
     gt_nii = gt.get_fdata()
     gt_nii = np.swapaxes(gt_nii, 0, 1)
     gt_nii = cv2.resize(gt_nii, (w, h))
 
-    seg = []
     y,x = gt_nii.shape[0], gt_nii.shape[1]
     for i in range(0,y):
         for j in range(0,x):
             if gt_nii[i][j] > 0:
                 seg.append(j)
                 seg.append(i)
-    
+        
     return gt_nii, seg
 
 
@@ -172,43 +172,66 @@ def is_crowd(mask):
 
 def get_bbox(mask):
     pos = np.where(mask)
-    xmin = np.min(pos[1])
-    xmax = np.max(pos[1])
-    ymin = np.min(pos[0])
-    ymax = np.max(pos[0])
+    xmin = np.min(pos[1]).item()
+    xmax = np.max(pos[1]).item()
+    ymin = np.min(pos[0]).item()
+    ymax = np.max(pos[0]).item()
     width = xmax - xmin
     height = ymax - ymin
-    return (width*height).item(), [xmin.item(), ymin.item(), width.item(), height.item()], xmax.item(), ymax.item()
+    return (width*height), [xmin, ymin, width, height]
 
 
 
-def split_dataset():
+# def split_dataset(mode, have_nifti):
+def split_dataset(mode, only_nifti):
     path = os.path.dirname(os.path.realpath(__file__))
     dcm_dir = path+"/dcm"
     nii_dir = path+"/nii"
     coco_dir = path+"/coco"
 
     img = []
+    null_annot = []
 
-    for i in os.listdir(nii_dir):
-        filename = i.split(".")[0]
-        dcm_file = f'{dcm_dir}/{filename}.dcm'
-        if os.path.exists(dcm_file):
+    for dcm in os.listdir(dcm_dir):
+        dcm_file = f'{dcm_dir}/{dcm}'
+        nii_file = f"{nii_dir}/{dcm.split('.')[0]}.nii.gz"
+        if os.path.exists(dcm_file) and os.path.exists(nii_file):
             img.append(dcm_file)
+        else:
+            null_annot.append(dcm_file)
+    
+    if only_nifti:
+        null_annot = []
+
+    random.shuffle(img)
+    random.shuffle(null_annot)
+
     #80 20
-    train, test = np.split(np.array(img),[int(len(img)* (.80))])
+    train, test = np.split(np.array(img),[int(len(img)*(.80))])
     #60 20
-    train, val = np.split(np.array(img),[int(len(img)* (.75))])
+    train, val = np.split(np.array(train),[int(len(train)*(.75))])
 
-    mean, std = to_coco_notation(train, "train")
-    to_coco_notation(val, "val")
-    to_coco_notation(test, "test")
+    null_annot_train, null_annot_test = np.split(np.array(null_annot),[int(len(null_annot)*(.80))])
+    #60 20
+    null_annot_train, null_annot_val = np.split(np.array(null_annot_train),[int(len(null_annot_train)*(.75))])
 
-    print(f'mean={mean}, std= {std}')
+    train_data = np.concatenate((train, null_annot_train))
+    val_data = np.concatenate((val, null_annot_val))
+    test_data = np.concatenate((test, null_annot_test))
+    random.shuffle(train_data)
+    random.shuffle(val_data)
+    random.shuffle(test_data)
+
+    print(len(train_data), len(val_data), len(test_data))
+
+    # mean, std = to_coco_notation(train_data, "train", mode)
+    to_coco_notation(val_data, "val", mode)
+    # to_coco_notation(test_data, "test", mode)
+
+    # print(f'mean={mean}, std= {std}')
 
 
-
-def to_coco_notation(img, used_for):
+def to_coco_notation(img, used_for, mode):
     coco_dataset = {
         "info": {
                 "description": "COCO Style Ankle Fracture Dataset",
@@ -228,7 +251,7 @@ def to_coco_notation(img, used_for):
         "images": [],
         "annotations": [],
         "categories": [
-            {"supercategory": "ankle", "id": 0, "name": "fracture"}
+            {"supercategory": "ankle", "id": 1, "name": "fracture"}
         ]
     }
     
@@ -240,15 +263,17 @@ def to_coco_notation(img, used_for):
     create_dir(f'{coco_dir}/{used_for}')
     
     multi_mask = []
-    img_id = 0
-    annot_id = 0
+    img_id = 1
+    annot_id = (len(img)*2)+1
     num_train_img = 0
 
     train_total_img_arr = np.zeros(dcm_to_png(img[0]).shape,np.float64)
-    
+    total = len(img)
+    file_num = 1
     for dcm_file in img:
         filename = dcm_file.split('/')[-1].split(".")[0]
-        print(f'working on: {filename}\n')
+        print(f'\n{file_num}/{total} of {used_for} working on: {filename}')
+        file_num += 1
         w, h, img_arr = make_img(dcm_file, coco_dir, used_for)
         
         if used_for == "train":
@@ -262,41 +287,67 @@ def to_coco_notation(img, used_for):
                 "height": h,
                 "width": w,
                 }
-        mask, segment = nii_to_seg(f'{nii_dir}/{filename}.nii.gz', w,h)
-        area, bbox, xmax, ymax = get_bbox(mask)
-
-        if not is_crowd(mask):
-            annotation = {
-                            "segmentation": [segment],
-                            "area": area,
-                            "iscrowd": 0,
-                            "image_id": img_id,
-                            "bbox": bbox,
-                            "category_id": 0,
-                            "id": annot_id
-                        }
-
-        else:
-            area, bbox, xmax, ymax = get_bbox(mask)
-            annotation = {
-                            "segmentation": rle(mask),
-                            "area": area,
-                            "iscrowd": 1,
-                            "image_id": img_id,
-                            "bbox": bbox,
-                            "category_id": 0,
-                            "id": annot_id
-                            }
         coco_dataset["images"].append(image)
-        coco_dataset["annotations"].append(annotation)
+
+        # https://blog.roboflow.com/missing-and-null-image-annotations/
+        nii_file = f'{nii_dir}/{filename}.nii.gz'
+        if os.path.exists(nii_file):
+            mask, segment = nii_to_seg(nii_file, w,h)
+            area, bbox = get_bbox(mask)
+            
+            if not is_crowd(mask):
+                if mode == "det":
+                    segment = []
+
+                annotation = {
+                                "segmentation": [segment],
+                                "area": area,
+                                "iscrowd": 0,
+                                "image_id": img_id,
+                                "bbox": bbox,
+                                "category_id": 1,
+                                "id": annot_id
+                            }
+            else:
+                area, bbox = get_bbox(mask)
+
+                if mode == "det":
+                    annotation = {
+                                    "segmentation": [[]],
+                                    "area": area,
+                                    "iscrowd": 1,
+                                    "image_id": img_id,
+                                    "bbox": bbox,
+                                    "category_id": 0,
+                                    "id": annot_id
+                                    }
+                elif mode == "seg":
+                    annotation = {
+                                    "segmentation": rle(mask),
+                                    "area": area,
+                                    "iscrowd": 1,
+                                    "image_id": img_id,
+                                    "bbox": bbox,
+                                    "category_id": 0,
+                                    "id": annot_id
+                                    }
+
+            coco_dataset["annotations"].append(annotation)
         img_id+=1
         annot_id+=1
 
     with open(f'{coco_dir}/{used_for}/annotation_coco.json', 'w') as f:
         json.dump(coco_dataset, f, ensure_ascii=False, indent=4)
-    
-    return np.mean(train_total_img_arr, axis=(0,1))/num_train_img, np.std(train_total_img_arr, axis=(0,1))/num_train_img
+    # return np.mean(train_total_img_arr, axis=(0,1))/num_train_img, np.std(train_total_img_arr, axis=(0,1))/num_train_img
 
 
-
-split_dataset()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    # det = bbox only
+    parser.add_argument("--mode", type=str, default= "det")
+    # only uses dcm with corresponding nifti
+    parser.add_argument("--nifti", action='store_true', default=False)
+    mode = parser.parse_args().mode
+    only_nifti = parser.parse_args().nifti
+    print(f'Creating {mode} CoCo dataset')
+    split_dataset(mode, only_nifti)
